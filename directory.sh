61f519143ac5b4acef7d315028a787ad621ae8f4
MrCharouon <ali.khatami2009@gmail.com>
2022-06-25 12:25:20 +0430

    moved the files and folders to tmp after we have been separated

---
+++ directory.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+dir="/tmp/watchlist/"
+if [  -d $dir  ];then
+#   echo ""Directory exists.""
+  x=1
+else
+  mkdir "/tmp/watchlist/"
+#   echo ""Directory Create.""
+fi
+# ls -ltrh $dir
